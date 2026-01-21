29f1259451ebf8057c6ea381889fec5eb4cb5ef9
yasuki314 <yasuki@juku-lets.com>
2026-01-21 22:38:38 +0900

    ver表示修正

--- docs/js/firebase-app.js
+++ docs/js/firebase-app.js
@@ -15,7 +15,7 @@ firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 
 // アプリバージョン
-const APP_VERSION = 'v1.3.1'; // v1.3.1に更新
+const APP_VERSION = 'v1.3.2'; // v1.3.2に更新
 window.APP_VERSION = APP_VERSION; // グローバルスコープでRoomManagerを使えるようにする
 
 document.addEventListener('DOMContentLoaded', () => {
